f1a3ada4de5cee28aa7e9eb78663819405333ed8
Ankush118 <100046763+Ankush118@users.noreply.github.com>
2022-02-26 01:06:37 +0530

    Add files via upload

---
+++ greater_of_2.py
@@ -0,0 +1,8 @@
+a=int(input("enter any number "))
+b=int(input("enter another any number "))
+if a>b:
+ print (a)
+if a==b:
+ print ("There is nothing complicated about equality")
+else:
+ print (b)
---
+++ greater_of_3.py
@@ -0,0 +1,13 @@
+a= int(input ("first number "))
+b= int(input ("second number "))
+c= int(input ("third number "))
+if a==b==c:
+ print ("Are you a fucking reatard or what?") 
+if a> b and a> c:
+ print (a)
+if b> a and b> c:
+ print (b)
+#else:
+ #print (c)
+if c> a and c> b:
+ print (c)
